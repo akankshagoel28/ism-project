f511bead719dcbb01d0f943a22a288b860582aa0
akankshagoel28 <akanksha.goel2020@vitstudent.ac.in>
2023-04-11 09:02:15 +0530

    build added

---
+++ frontend/build/precache-manifest.fe505b06166fb6a4ed81fb3be984c574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566cec9c775e2c38b117effcd3b37238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbae53bfe097281d5587",
+    "url": "/static/css/main.83731651.chunk.css"
+  },
+  {
+    "revision": "773e077eb7373c9924bc",
+    "url": "/static/js/2.f8de7644.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f8de7644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbae53bfe097281d5587",
+    "url": "/static/js/main.cae014cc.chunk.js"
+  },
+  {
+    "revision": "940b9cb0c27bb71a470a",
+    "url": "/static/js/runtime-main.fc5eeba3.js"
+  }
+]);
